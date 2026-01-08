dc646fd275125e0fbddf16827162c5dabec10939
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2026-01-07 23:15:16 -0300

    style: adiciona logo

--- frontend/algorithm-visualizer/src/app/layouts/full/sidebar/branding.component.ts
+++ frontend/algorithm-visualizer/src/app/layouts/full/sidebar/branding.component.ts
@@ -6,10 +6,13 @@ import { CoreService } from 'src/app/services/core.service';
   selector: 'app-branding',
   imports: [],
   template: `
-    <div class="branding d-none d-lg-flex align-items-center">
-      <h1 (click)="goToHomePage()" style="cursor:pointer" >Algorithm Visualizer</h1>
-    </div>
-  `,
+  <div class="branding d-none d-lg-flex align-items-center"
+   (click)="goToHomePage()" 
+       style="cursor: pointer;">
+    <img src="assets/favicon.ico" alt="Logo" width="50" height="50" style="margin-right: 7px;">
+    <h1 style="color: white">Algorithm Visualizer</h1>
+  </div>
+`,
 })
 export class BrandingComponent {
   options = this.settings.getOptions();
