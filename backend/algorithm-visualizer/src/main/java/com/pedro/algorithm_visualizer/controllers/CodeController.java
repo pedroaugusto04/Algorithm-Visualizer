52306914b5af8b9a8ad2679ed717db989324b17f
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2026-01-31 23:07:48 -0300

    chore: ajusta dockerfile

--- backend/algorithm-visualizer/src/main/java/com/pedro/algorithm_visualizer/controllers/CodeController.java
+++ backend/algorithm-visualizer/src/main/java/com/pedro/algorithm_visualizer/controllers/CodeController.java
@@ -1,6 +1,5 @@
 package com.pedro.algorithm_visualizer.controllers;
 
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
