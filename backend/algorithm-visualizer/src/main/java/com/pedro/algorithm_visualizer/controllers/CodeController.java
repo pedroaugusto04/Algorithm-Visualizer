63dc786e6141900bb6707264f8134351e4c770a2
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2026-01-31 23:02:04 -0300

    chore: ajusta dockerfile

--- backend/algorithm-visualizer/src/main/java/com/pedro/algorithm_visualizer/controllers/CodeController.java
+++ backend/algorithm-visualizer/src/main/java/com/pedro/algorithm_visualizer/controllers/CodeController.java
@@ -1,16 +1,21 @@
 package com.pedro.algorithm_visualizer.controllers;
 
-import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pedro.algorithm_visualizer.models.DTO.CodeRequestDTO;
 import com.pedro.algorithm_visualizer.models.DTO.ExecutionResponseDTO;
 import com.pedro.algorithm_visualizer.services.CodeService;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/code")
